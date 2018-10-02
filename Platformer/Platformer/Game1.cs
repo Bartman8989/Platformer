adc9b3dd8f3e6ab07e60ef1e57380a080d34d64a
Sir_Bart <c_barts@live.com>
2018-10-02 14:56:46 +1000

    Added Map
    added code for the map to be generated and the files needed to do so

--- Platformer/Platformer/Game1.cs
+++ Platformer/Platformer/Game1.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Graphics;
+using MonoGame.Extended.ViewportAdapters;
 
 namespace Platformer
 {
@@ -14,7 +18,9 @@ namespace Platformer
 
         Player player = new Player(); //Create an instance of our player class
 
-
+        Camera2D camera = null; //Create an instance of a 2D Camera
+        TiledMap map = null; //Creates an instance of a Tiled map
+        TiledMapRenderer mapRenderer = null; //Creates an instance ofwhat makes a Tiled map
 
         public Game1()
         {
@@ -45,6 +51,14 @@ namespace Platformer
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             player.Load(Content); // Call the 'Load' function in the Player class
+
+            BoxingViewportAdapter viewportAdapter = new BoxingViewportAdapter(Window,GraphicsDevice, graphics.GraphicsDevice.Viewport.Width, graphics.GraphicsDevice.Viewport.Height);
+            camera = new Camera2D(viewportAdapter);
+            camera.Position = new Vector2(0, graphics.GraphicsDevice.Viewport.Height);
+
+            map = Content.Load<TiledMap>("Level1");
+            mapRenderer = new TiledMapRenderer(GraphicsDevice);
+
         }
 
         /// <summary>
@@ -82,8 +96,18 @@ namespace Platformer
         {
             // CLear anything previously drawn to the screen
             GraphicsDevice.Clear(Color.Gray);
+
+            var viewMatrix = camera.GetViewMatrix();
+            var projectionMatrix = Matrix.CreateOrthographicOffCenter(0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 0, 0.0f, -1.0f);
+
+
+
             //Begin drawing
-            spriteBatch.Begin();
+            spriteBatch.Begin(transformMatrix: viewMatrix);
+
+
+
+            mapRenderer.Draw(map, ref viewMatrix, ref projectionMatrix);
             // Call the 'Draw' function from Player class
             player.Draw(spriteBatch);
             //Finish drawing
